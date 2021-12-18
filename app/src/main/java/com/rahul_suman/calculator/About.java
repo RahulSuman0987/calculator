c45e1ba8aa5e4e63c7768d82abde7dc098a5af85
Srijan Kumar Sah <rahulsuman.sonu@gmail.com>
2021-12-18 20:04:04 +0530

    first commit

---
+++ app/src/main/java/com/rahul_suman/calculator/About.java
@@ -0,0 +1,15 @@
+package com.rahul_suman.calculator;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android_scientific_calculator.R;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+
+    }
+}
